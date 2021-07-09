0eb9f52179cc5e370c04138e4ee1a237cb4d3a4e
slutske22 <seth.lutske@gmail.com>
2021-07-08 21:42:33 -0700

    Setting up basic react project with webpack and babel

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# ui
-Firestarter front end code
+# Debug UI
+
+This repo contains a front end application built on react and leaflet. This UI is currently acting as a debug UI to use for testing in developing the core FireStarter algorithm.
---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(
+	<React.StrictMode>
+		<div>Yup!</div>
+	</React.StrictMode>,
+	rootElement
+);
